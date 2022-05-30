7c88603bbbe559a3b2f7f89a842a82ba1e6d2e23
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2022-05-30 13:23:51 +1200

    5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use num::complex;
+use fltk::{frame, image, enums, app, window};
 use fltk::prelude::{WidgetBase, GroupExt, WidgetExt, WindowExt};
 
-fn render_mandelbrot(frame: &mut fltk::frame::Frame/*, window_width: u32, window_height: u32*/, width: f64, aspect_ratio: f64, centre: num::complex::Complex<f64>, max_iter: u64) {
+fn render_mandelbrot(frame: &mut frame::Frame, width: f64, aspect_ratio: f64, centre: complex::Complex<f64>, max_iter: u64) {
 	// Initial calculations
 	let window = frame.parent().unwrap();
 	let window_width = window.width() as u32;
@@ -9,23 +11,27 @@ fn render_mandelbrot(frame: &mut fltk::frame::Frame/*, window_width: u32, window
 	let screen_aspect_ratio = (window_width as f64) / (window_height as f64);
 	let applied_aspect_ratio = aspect_ratio / screen_aspect_ratio;
 	
-	// Render (not draw)
+	// Render
 	let mut data: Vec<u8> = vec![0; window_width as usize * window_height as usize];
+	let mut c = complex::Complex::new(0., 0.);
 	for y in 0..window_height {
+		c.im = ((y as f64) / (window_height as f64) - 0.5) * applied_aspect_ratio * width + centre.im;
 		for x in 0..window_width {
-			let c = num::complex::Complex::new((x as f64) / (window_width as f64) - 0.5, ((y as f64) / (window_height as f64) - 0.5) * applied_aspect_ratio) * width + centre;
-			let mut z = num::complex::Complex::new(0f64, 0f64);
+			c.re = ((x as f64) / (window_width as f64) - 0.5) * width + centre.re;
+			let mut z = complex::Complex::new(0f64, 0f64);
 			let mut iter_count: u64 = 0;
-			while z.norm() < 2f64 && iter_count < max_iter {
+			while z.re * z.re + z.im * z.im < 4. && iter_count < max_iter {
+				let re_temp = z.re * z.re - z.im * z.im + c.re;
+				z.im = 2. * z.re * z.im + c.im;
+				z.re = re_temp;
 				iter_count += 1;
-				z = z * z + c;
 			}
 			data[(y as usize) * (window_width as usize) + (x as usize)] = ((iter_count as f64).log(max_iter as f64) * 256.) as u8;
 		}
 	}
 
 	// Draw
-	let image = fltk::image::RgbImage::new(&data, window_width as i32, window_height as i32, fltk::enums::ColorDepth::L8);
+	let image = image::RgbImage::new(&data, window_width as i32, window_height as i32, enums::ColorDepth::L8);
 	frame.set_image(image.ok());
 }
 
@@ -33,22 +39,22 @@ fn main() {
 	// Vars
 	let mut pixel_width: u32 = 640;
 	let mut pixel_height: u32 = 480;
-	let centre = num::complex::Complex::new(0., 0.);
+	let centre = complex::Complex::new(0., 0.);
 	let width = 4.;
 	let aspect_ratio = 1.;
 	let max_iter: u64 = 256;
 
 	// Calculation
-	let app = fltk::app::App::default();
-	let mut window = fltk::window::Window::new(0, 0, pixel_width as i32, pixel_height as i32, "Mandelbrot").center_screen();
+	let app = app::App::default();
+	let mut window = window::Window::new(0, 0, pixel_width as i32, pixel_height as i32, "Mandelbrot").center_screen();
 	window.make_resizable(true);
-	let mut frame = fltk::frame::Frame::default().center_of(&window);
+	let mut frame = frame::Frame::default().center_of(&window);
 	render_mandelbrot(&mut frame, width, aspect_ratio, centre, max_iter);
 
 	// Display
 	window.end();
 	frame.handle(move |frame, event| match event {
-		fltk::enums::Event::Resize => {
+		enums::Event::Resize => {
 			let window = frame.parent().unwrap();
 			let new_width = window.width() as u32;
 			let new_height = window.height() as u32;
